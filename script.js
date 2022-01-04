931359ea5034293570d401c480130abefde21f6b
MyoMyintAung <myomyintaung064@gmail.com>
2022-01-04 11:00:20 +0630

    Finish modal

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtns = document.querySelectorAll('.show-modal');
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+for (let i = 0; i < showBtns.length; i++) {
+  showBtns[i].addEventListener('click', openModal);
+}
